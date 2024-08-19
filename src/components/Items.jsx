e691fae7c6e850b3749e062ee9d3f0c4681ff568
Shawn Smith <sds.smith24@gmail.com>
2024-08-19 12:42:12 -0400

    commit change

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -2,14 +2,20 @@
 import { Button, VStack, useDisclosure } from '@chakra-ui/react'
 import ItemCard from './ItemCard';
 import EditItemModal from './EditItemModal';
+import { useAuthContext } from '../context/AuthContext';
+import { useFamilyMemberParam } from '../hooks/useFamilyMemberParam';
 
 export default function Items({ items }) {
     const { isOpen, onOpen, onClose } = useDisclosure();
+    const { currentUser } = useAuthContext();
+    const selectedFamilyMember = useFamilyMemberParam();
+
+    const userSelectedSelf = currentUser?.uid === selectedFamilyMember?.uid;
 
     return (
         <>
             <VStack spacing={4} align='start'>
-                <Button colorScheme='blue' onClick={onOpen}>Add Item</Button>
+                { userSelectedSelf && <Button colorScheme='blue' onClick={onOpen}>Add Item</Button> }
                 <VStack spacing={4}>
                     { items?.map(item => (
                         <ItemCard key={item.name} item={item} />
