5d05daa8349f8edde8eca8d8d1fef077fb047eee
Shawn Smith <sds.smith24@gmail.com>
2025-01-18 11:07:36 -0500

    borderColor

--- src/components/ItemCardBase.jsx
+++ src/components/ItemCardBase.jsx
@@ -15,6 +15,25 @@ import { useAuthContext } from '../context/AuthContext';
 import { useFamilyMemberParam } from '../hooks/useFamilyMemberParam';
 import MyItemControls from './MyItemControls';
 
+const borderColor = {
+  lightModeValue: {
+    1:  'cyan.100',
+    2:  'teal.100',
+    3:  'green.200',
+    4:  'yellow.200',
+    5:  'orange.200',
+    10: 'blackAlpha.50'
+  },
+  darkModeValue: {
+    1:  'cyan.700',
+    2:  'teal.700',
+    3:  'green.700',
+    4:  'yellow.700',
+    5:  'orange.700',
+    10: 'whiteAlpha.200'
+  }
+}
+
 export default function ItemCardBase({item}) {
   const { currentUser } = useAuthContext();
   const selectedFamilyMember = useFamilyMemberParam();
@@ -22,31 +41,36 @@ export default function ItemCardBase({item}) {
   const isParent = currentUser?.role === 'parent';
   const userSelectedSelf = currentUser?.uid === selectedFamilyMember?.uid;
 
-    return (
-      <Card w='xs' bg={useColorModeValue('blackAlpha.50', 'whiteAlpha.200')} >
-        <CardHeader>
-          <Flex spacing='4'>
-            <Flex flex='1' gap='4' alignItems='center' flexWrap='wrap'>
-              <Box>
-                <Heading size='md' >{item?.name}</Heading>
-                { item?.priority <= 5 &&
-                  <Heading size='sm' fontWeight='semibold'>{`Priority Level: ${item?.priority}`}</Heading> 
-                }
-                { Boolean(item.url) && (
-                  <Text>
-                    <LinkOverlay href={item.url} target='_blank' rel='no-referrer'>{item.url.split('/')[2]}</LinkOverlay>
-                  </Text>
-                )}
-              </Box>
-            </Flex>
+  return (
+    <Card 
+      w='xs' 
+      bg={useColorModeValue('blackAlpha.50', 'whiteAlpha.200')} 
+      borderWidth={'2px'}
+      borderColor={useColorModeValue(borderColor.lightModeValue[item.priority], borderColor.darkModeValue[item.priority])}
+    >
+      <CardHeader>
+        <Flex spacing='4'>
+          <Flex flex='1' gap='4' alignItems='center' flexWrap='wrap'>
+            <Box>
+              <Heading size='md' >{item?.name}</Heading>
+              { item?.priority <= 5 &&
+                <Heading size='sm' fontWeight='semibold'>{`Priority Level: ${item?.priority}`}</Heading> 
+              }
+              { Boolean(item.url) && (
+                <Text>
+                  <LinkOverlay href={item.url} target='_blank' rel='no-referrer'>{item.url.split('/')[2]}</LinkOverlay>
+                </Text>
+              )}
+            </Box>
           </Flex>
-        </CardHeader>
-        <CardBody>
-          <Box>
-            <Text>{`${item?.comment || ''}`}</Text>
-            { userSelectedSelf ? <MyItemControls item={item} /> : <PurchaseWidget item={item} /> }
-          </Box>
-        </CardBody>
-      </Card>
-    )
-  }
+        </Flex>
+      </CardHeader>
+      <CardBody>
+        <Box>
+          <Text>{`${item?.comment || ''}`}</Text>
+          { userSelectedSelf ? <MyItemControls item={item} /> : <PurchaseWidget item={item} /> }
+        </Box>
+      </CardBody>
+    </Card>
+  )
+}
