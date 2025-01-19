be2028ea6fd91bf5a75e5d406418cb7ba494c19b
Shawn Smith <sds.smith24@gmail.com>
2025-01-19 11:14:27 -0500

    configure color mode in theme

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ColorModeScript } from '@chakra-ui/react'
 import { ChakraProvider } from '@chakra-ui/react'
 import { AuthProvider } from './context/AuthContext.jsx'
 import App from './App.jsx'
+import theme from './lib/theme.js'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ChakraProvider>
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+    <ChakraProvider theme={theme}>
       <AuthProvider>
         <App />
       </AuthProvider>
