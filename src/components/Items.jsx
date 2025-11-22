040b60be0a54145b5af156d17e4b07d96abc8825
Shawn Smith <sds.smith24@gmail.com>
2025-11-22 12:43:05 -0500

    group by category

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -1,34 +1,20 @@
-import { useState } from 'react';
-import Button from "@mui/material/Button";
+
 import Stack from "@mui/material/Stack";
-
+import Typography from "@mui/material/Typography";
 import ItemCard from './ItemCard';
-import EditItemModal from './EditItemModal';
-import { useAuthContext } from '../context/AuthContext';
-import { useFamilyMemberParam } from '../hooks/useFamilyMemberParam';
 
-export default function Items({ items }) {
-    const { currentUser } = useAuthContext();
-    const selectedFamilyMember = useFamilyMemberParam();
-
-  const [ isOpenEdit, setIsOpenEdit ] = useState(false);
-
-  const onOpenEdit = () => setIsOpenEdit(true)
-  const onCloseEdit = () => setIsOpenEdit(false)
-
-    const userSelectedSelf = currentUser?.uid === selectedFamilyMember?.uid;
-
+export default function Items({ category, items }) {
+    const prioritized = items?.sort((a, b) => (a.priority || 10) - (b.priority || 10))
     return (
-        <>
+        <div style={{marginTop: '2rem'}}>
+            <Typography variant="h5">{category}</Typography>
             <Stack spacing={4} align='start'>
-                { userSelectedSelf && <Button variant="contained" onClick={onOpenEdit}>Add Item</Button> }
                 <Stack spacing={4}>
-                    { items?.map(item => (
+                    { prioritized?.map(item => (
                         <ItemCard key={item.name} item={item} />
                     ))}
                 </Stack>
             </Stack>
-            <EditItemModal action='Add' item={{}} isOpen={isOpenEdit} onClose={onCloseEdit} />
-        </>
+        </div>
       )
 }
--- src/pages/FamilyMember.jsx
+++ src/pages/FamilyMember.jsx
@@ -1,18 +1,66 @@
+import { useState, useMemo } from 'react';
 import { Link } from 'react-router-dom';
+import Button from "@mui/material/Button";
 import Typography from '@mui/material/Typography';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
 import Items from '../components/Items';
+import EditItemModal from '../components/EditItemModal';
+import { useAuthContext } from '../context/AuthContext';
 import { useFamilyMemberParam } from '../hooks/useFamilyMemberParam';
 
 export default function FamilyMember() {
-    const { displayName, items } = useFamilyMemberParam();
+    const { currentUser } = useAuthContext();
+    const selectedFamilyMember = useFamilyMemberParam();
+    const { displayName, items } = selectedFamilyMember;
 
-    const prioritizedItems = items?.sort ((a, b) => a.priority - b.priority)
+    const itemsByCategory = useMemo(() => items?.reduce((acc, curr) => ({
+      ...acc,
+      [curr.category || "No Category"] : [...(acc[curr.category || "No Category"] || []), curr]
+    }), {}) || {}, [items]);
+
+    const allCategories = useMemo(() => Object.keys(itemsByCategory), [itemsByCategory]);
+
+    const [ selectedCategories, setSelectedCategories ] = useState([]);
+    const [ isOpenEdit, setIsOpenEdit ] = useState(false);
+
+    const onOpenEdit = () => setIsOpenEdit(true)
+    const onCloseEdit = () => setIsOpenEdit(false)
+
+    const userSelectedSelf = currentUser?.uid === selectedFamilyMember?.uid;
+
+    const handleSelect = (e) => setSelectedCategories(e.target.value);
 
     return (
       <>
         <Link to='/home' style={{textDecoration: 'none', color: 'black'}}>{'< Home'}</Link>
-        <Typography>{displayName}</Typography>
-        <Items items={items} />
+        <Typography variant="h4">{displayName}</Typography>
+        { userSelectedSelf && <Button variant="contained" onClick={onOpenEdit}>Add Item</Button> }
+        <FormControl fullWidth sx={{marginTop: '1rem'}}>
+          <InputLabel id="demo-multiple-checkbox-label" >Select Categories</InputLabel>
+          <Select
+            fullWidth
+            labelId="demo-multiple-checkbox-label"
+            multiple
+            value={selectedCategories}
+            onChange={handleSelect}
+            input={<OutlinedInput label="Select Categories" />}
+          >
+            { allCategories.map(c => (
+              <MenuItem key={c} value={c}>{c}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        { !selectedCategories.length 
+          ? <Items key={'all'} category={"All Items"} items={items} />
+          : selectedCategories.map(cat => (
+              <Items key={cat} category={cat} items={itemsByCategory[cat]} />
+          ))
+        }
+        <EditItemModal action='Add' item={{}} isOpen={isOpenEdit} onClose={onCloseEdit} />
       </>
     )
 }
